c4ddce01706457e52198ec3605acabf51e559a83
slyer20174 <edushiloh1@gmail.com>
2024-12-15 17:42:09 +0000

    Add files via upload

---
+++ maps.py
@@ -0,0 +1,4 @@
+list1=[6,3,7,9]
+list2=[3,9,6,4]
+result=map(lambda x, y: x+y, list1,list2)
+print(list(result))
---
+++ zip.py
@@ -0,0 +1,4 @@
+set1={1,2,3,4}
+set2={"a","b","c","d"}
+set3=list(zip(set1,set2))
+print(set3)
